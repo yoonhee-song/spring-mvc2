0d8212d8044de461e5f39069354facabdee93b8a
yoonhee-song <67790056+yoonhee-song@users.noreply.github.com>
2022-08-06 23:38:28 +0900

    Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+//@ScriptAssert: 오브젝트 관련 오류 처리해주는 어노테이션. but 실무에서 사용 시 어려움이 많고 복집하기 때문에 지양 -> 오브젝트 관련 오류는 자바 코드로 작성
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "10000원 넘게 입력해주세요.")
 @Data
 public class Item {
 
