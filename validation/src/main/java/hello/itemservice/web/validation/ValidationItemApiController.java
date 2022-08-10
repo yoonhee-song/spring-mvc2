0e7389f493c31bff30296548274173ac332b0875
yoonhee-song <67790056+yoonhee-song@users.noreply.github.com>
2022-08-10 22:34:29 +0900

    Bean Validation - HTTP 메시지 컨버터

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -13,7 +13,11 @@ public class ValidationItemApiController {
 
     @PostMapping("/add")
     public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
-
+        /*
+            성공 요청: 성공
+            실패 요청: JSON을 객체로 생성하는 것 자체가 실패(HttpMessageConverter 자체가 실패). 따라서 controller 호출 자체를 안함
+            검증 오류 요청: JSON을 객체로 생성하는 것은 성공했고, 검증에서 실패. controller 호출은 성공
+        */
         log.info("API 컨트롤러 호출");
 
         if (bindingResult.hasErrors()) {
