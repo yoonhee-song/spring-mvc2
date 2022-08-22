65bdfa7ffc0f2f8fe19715c0b4c2bccc5d219470
yoonhee-song <67790056+yoonhee-song@users.noreply.github.com>
2022-08-22 23:36:29 +0900

    로그인 기능

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,25 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *
+     * @param password
+     * @param loginId
+     * @return null 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
