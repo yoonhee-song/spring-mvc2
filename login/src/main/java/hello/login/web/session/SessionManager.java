88316532136706c024030d2141ad60d9b389dc85
yoonhee-song <67790056+yoonhee-song@users.noreply.github.com>
2022-08-26 22:55:00 +0900

    로그인 처리하기 - 세션 직접 만들기

--- login/src/main/java/hello/login/web/session/SessionManager.java
+++ login/src/main/java/hello/login/web/session/SessionManager.java
@@ -3,7 +3,9 @@ package hello.login.web.session;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,10 +17,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SessionManager {
 
     public static final String SESSION_COOKIE_NAME = "mySessionId";
+    //동시성 문제가 있을 땐 ConcurrentHashMap을 사용해 주는 것이 좋음(세션 동시다발적 접급)
     private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
 
     /**
      * 세션 생성
+     * * sessionId 생성 (임의의 추정 불가능한 랜덤 값)
+     * * 세션 저장소에 sessionId와 보관할 값 저장
+     * * sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
      */
     public void createSession(Object value, HttpServletResponse response) {
 
@@ -29,5 +35,36 @@ public class SessionManager {
         //쿠키 생성
         Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
         response.addCookie(mySessionCookie);
+    }
+
+    /**
+     * 세션 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
     }
 }
