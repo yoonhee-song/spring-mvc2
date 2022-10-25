db143f14752e32e9c83d7005158aba849dd28b18
yoonhee-song <67790056+yoonhee-song@users.noreply.github.com>
2022-10-25 22:53:20 +0900

    commit

--- exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -4,12 +4,14 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class ApiExceptionV2Controller {
-    @GetMapping("/api2/members/{id}")
+public class ApiExceptionV3Controller {
+    @GetMapping("/api3/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
 
         if (id.equals("ex")) {
